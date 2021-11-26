3cd8abfa6a4ef3a3ec84ddd21f6be00640a0330a
Denis Khizhin <denisk@backbase.com>
2021-11-26 16:40:00 +0100

    feat: implement dao

--- app/src/main/java/com/codingwithmitch/food2forkcompose/cash/database/AppDatabase.kt
+++ app/src/main/java/com/codingwithmitch/food2forkcompose/cash/database/AppDatabase.kt
@@ -2,11 +2,12 @@ package com.codingwithmitch.food2forkcompose.cash.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.codingwithmitch.food2forkcompose.cash.RecipeDao
 import com.codingwithmitch.food2forkcompose.cash.model.RecipeEntity
 
 @Database(entities = [RecipeEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun recipeDAO(): RecipeDAO
+    abstract fun recipeDao(): RecipeDao
 
     companion object {
         val DATABASE_NAME = "recipe_db"
